0079bf5c54cc08ca622b657b0945087d838d6d97
FaizanSyed <faizansyed1996@gmail.com>
2016-12-20 00:26:20 -0500

    Made minor UI changes to MainActivity

--- app/src/main/java/com/example/faizan/notextzone/MainActivity.java
+++ app/src/main/java/com/example/faizan/notextzone/MainActivity.java
@@ -4,17 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     SmsManager smsManager;
-    EditText message;
     Button sendButton;
+    Switch drivingSwitch;
     private static final String TAG = "SEND";
 
     @Override
@@ -22,15 +23,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        message = (EditText) findViewById(R.id.message);
-        sendButton = (Button) findViewById(R.id.sendButton);
         smsManager = SmsManager.getDefault();
+        sendButton = (Button) findViewById(R.id.sendButton);
+        drivingSwitch = (Switch) findViewById(R.id.drivingSwitch);
+
+        drivingSwitch.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(isChecked){
+                            Toast.makeText(MainActivity.this, "Driving mode is ON!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Driving mode is OFF!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    smsManager.sendTextMessage("5195807115", null, "HEY THERE BUD", null, null);
+                    smsManager.sendTextMessage("16477021419", null, "HEY THERE BUD", null, null);
                 } catch (SecurityException se){
                     Log.i(TAG, "Don't have permission!");
                     Toast.makeText(MainActivity.this, "DON'T HAVE PERMISSION", Toast.LENGTH_SHORT).show();
